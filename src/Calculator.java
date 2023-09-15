c4b51e70d614f081d518db8dbae3ecbb628c659f
Dav1200 <davinder.singh@city.ac.uk>
2023-09-15 15:38:03 +0100

    Action listener to buttons added

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
 
-public class Calculator {
-    private JPanel jpanel;
+public class Calculator extends JFrame {
+    private JPanel mainPanel;
     private JButton b0;
-
     private JButton b1;
-
     private JButton b2;
     private JButton b3;
     private JButton b4;
@@ -22,12 +23,28 @@ public class Calculator {
     private JButton ACButton;
     private JButton bdot;
     private JButton bequals;
+    private JLabel displayAnswer;
+    private JPanel displayQuestion;
+    private JLabel question;
 
     Calculator() {
 
+        this.setContentPane(this.mainPanel);
+        this.setSize(600,400);
+        this.setVisible(true);
+displayAnswer.setText("123123123");
+question.setText("123123123");
+
+        b0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"0");
+            }
+        });
     }
 
     public static void main(String[] args) {
+        Calculator a = new Calculator();
 
     }
 }
