17aa832a220b2867e39cbead359d9da0d1582169
Dav1200 <davinder.singh@city.ac.uk>
2023-09-15 15:13:23 +0100

    GUI for calculator created

---
+++ src/Calculator.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class Calculator {
+    private JPanel jpanel;
+    private JButton b0;
+
+    private JButton b1;
+
+    private JButton b2;
+    private JButton b3;
+    private JButton b4;
+    private JButton b5;
+    private JButton b6;
+    private JButton b7;
+    private JButton b8;
+    private JButton b9;
+    private JButton bplus;
+    private JButton bminus;
+    private JButton btimes;
+    private JButton bdivide;
+    private JButton backspaceb;
+    private JButton ACButton;
+    private JButton bdot;
+    private JButton bequals;
+
+    Calculator() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
