e07ebc8bde8048bf28633c2418e707e62992e413
Dav1200 <davinder.singh@city.ac.uk>
2023-09-15 16:18:27 +0100

    Fully functional calculator used expression library

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,12 @@
+import tk.pratanumandal.expr4j.ExpressionEvaluator;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+
+
 import java.awt.event.FocusAdapter;
 
 public class Calculator extends JFrame {
@@ -32,13 +38,133 @@ public class Calculator extends JFrame {
         this.setContentPane(this.mainPanel);
         this.setSize(600,400);
         this.setVisible(true);
-displayAnswer.setText("123123123");
-question.setText("123123123");
+
 
         b0.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 question.setText(question.getText()+"0");
+            }
+        });
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"1");
+            }
+        });
+        b4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"4");
+            }
+        });
+        b7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"7");
+            }
+        });
+        bdot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+".");
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"2");
+            }
+        });
+        b5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"5");
+            }
+        });
+        b8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"8");
+            }
+        });
+        b3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"3");
+            }
+        });
+        b6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"6");
+            }
+        });
+        b9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"9");
+            }
+        });
+        bplus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"+");
+            }
+        });
+        bequals.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+
+
+                String expr = question.getText().toString();
+                ExpressionEvaluator exprEval = new ExpressionEvaluator();
+                double result = exprEval.evaluate(expr);
+                displayAnswer.setText(String.valueOf(result));
+                }
+                catch (Exception a ){
+                    System.out.println("Invalid Expression");
+                    JOptionPane.showMessageDialog(mainPanel, "Invalid Expression ");
+                    question.setText("");
+                    displayAnswer.setText("");
+                }
+
+
+
+
+            }
+        });
+        bminus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"-");
+            }
+        });
+        btimes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"*");
+            }
+        });
+        bdivide.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText()+"/");
+            }
+        });
+        ACButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                question.setText("");
+                displayAnswer.setText("");
+            }
+        });
+        backspaceb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                question.setText(question.getText().substring(0,question.getText().length()-1));
             }
         });
     }
